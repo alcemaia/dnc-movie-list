72280e27255ab980ae3d06a71a8381331e330095
AlceMaia <alceniziamaia@gmail.com>
2023-04-27 20:55:31 -0300

    Adicionei um comentário no movieService

--- src/api/MovieService.js
+++ src/api/MovieService.js
@@ -4,10 +4,10 @@ const BASE_URL = "http://api.themoviedb.org/3/";
 const API_KEY = "3e074b6d14a7158d77bae02b97da066e";
 
 
-const withBaseUrl = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`; 
+const withBaseUrl = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`; /*string que é o endpoint dinamico*/
 
 export class MovieService {
-//método que vai buscar os filmes
+//método que vai buscar os filmes - essa classe não vai construir um objeto, vai executar metodos.
     static getMovies() {
         return axios(withBaseUrl("movie/popular"));
     }
