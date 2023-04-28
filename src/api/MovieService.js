d95afece8a8e45ec5285ce2d80abd82c40cd52be
AlceMaia <alceniziamaia@gmail.com>
2023-04-28 08:33:16 -0300

    alteração de constante para https

--- src/api/MovieService.js
+++ src/api/MovieService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_HTTP = "http://api.themoviedb.org/3/";
+const BASE_HTTPS = "http://api.themoviedb.org/3/";
 const API_KEY = "3e074b6d14a7158d77bae02b97da066e";
 
 
-const withBaseHttp = (path) => `${BASE_HTTP}${path}?api_key=${API_KEY}`; /*string que é o endpoint dinamico*/
+const withBaseHttps = (path) => `${BASE_HTTPS}${path}?api_key=${API_KEY}`; /*string que é o endpoint dinamico*/
 
 export class MovieService {
 //método que vai buscar os filmes - essa classe não vai construir um objeto, vai executar metodos.
