7f6452db856a3d916bf74b8d9c4e3ff96379eb48
AlceMaia <alceniziamaia@gmail.com>
2023-04-28 09:42:33 -0300

    alteração no withbaseURL

--- src/api/MovieService.js
+++ src/api/MovieService.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-const BASE_HTTPS = "http://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "3e074b6d14a7158d77bae02b97da066e";
 
 
-const withBaseHttps = (path) => `${BASE_HTTPS}${path}?api_key=${API_KEY}`; /*string que é o endpoint dinamico*/
+const withbaseURL = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`; /*string que é o endpoint dinamico*/
 
 export class MovieService {
 //método que vai buscar os filmes - essa classe não vai construir um objeto, vai executar metodos.
     static getMovies() {
-        return axios(http("movie/popular"));
+        return axios(withbaseURL("movie/popular"));
     }
 
     static getMovieDetails(id) {
-        return axios(http(`movie/${id}`));
+        return axios(withbaseURL(`movie/${id}`));
     }
 
     static searchMovies(movie) {
-        return axios(http("search/movie") + `&query=${movie}`);
+        return axios(withbaseURL("search/movie") + `&query=${movie}`);
 
     }
 }
